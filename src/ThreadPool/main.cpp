eac6be93c7cac57d6e43d921f85e27401178442a
omarosamahu <omarosama255@gmail.com>
2023-05-26 15:41:04 +0300

    Git modify

--- src/ThreadPool/main.cpp
+++ src/ThreadPool/main.cpp
@@ -3,16 +3,6 @@
 int main(int argc, char const *argv[])
 {
     
-    ThreadPool th1(1);
-    for(int i =0 ; i < 100 ;i++){
-        th1.addTasks([i](){
-            // std::this_thread::sleep_for(std::chrono::milliseconds(200));
-            std::cout << (i%2 == 0 ? "Thread1" : "Thread2")  << "\n";
-        });
-
-    // th1.wait();
-    }
-    // th1.wait();
-    
+    std::cout << "Hello Yocto git\n";
     return 0;
 }
